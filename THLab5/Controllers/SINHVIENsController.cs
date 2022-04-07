0f92d713b5cb26d291700dcc6d5a2b9966a56069
kt_wi <kt_wi@DESKTOP-VFRP04T>
2022-04-07 22:36:55 +0700

    Add project files.

---
+++ THLab5/Controllers/SINHVIENsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using THLab5.Models;
+
+namespace THLab5.Controllers
+{
+    public class SINHVIENsController : Controller
+    {
+        private qlsvEntities db = new qlsvEntities();
+
+        // GET: SINHVIENs
+        public ActionResult Index()
+        {
+            return View(db.SINHVIENs.ToList());
+        }
+
+        // GET: SINHVIENs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SINHVIENs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSV,Hoten,NamSinh,Dantoc,Khoahoc,Khoa,Chuongtrinh,STTLop,SLSV")] SINHVIEN sINHVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SINHVIENs.Add(sINHVIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sINHVIEN);
+        }
+
+        // POST: SINHVIENs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaSV,Hoten,NamSinh,Dantoc,Khoahoc,Khoa,Chuongtrinh,STTLop,SLSV")] SINHVIEN sINHVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sINHVIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sINHVIEN);
+        }
+
+        // GET: SINHVIENs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sINHVIEN);
+        }
+
+        // POST: SINHVIENs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SINHVIEN sINHVIEN = db.SINHVIENs.Find(id);
+            db.SINHVIENs.Remove(sINHVIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ THLab5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="THLab5.MvcApplication" Language="C#" %>
